c63156c65ec0c1b6c57154313e7b336e7fab19ba
carmenkilbourne <113438693+carmenkilbourne@users.noreply.github.com>
2025-09-24 12:41:34 +0200

    added endpoint get

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
  
  1. Set the environment that i'm going to be working in, in this case 
  i'm using flask.
+ 2. Created mock data to see if my endpoint works correctly.
--- main.py
+++ main.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def main():
     return "Main"
 
+@app.route("/subscriptions/<subscription_id>")
+def get_subscription(subscription_id):
+    subscription_data = {
+        "id": subscription_id,
+        "email": "carmen.kilbourne@gmail.com",
+        "name": "Carmen",
+        "plan": "standard"
+    }
+    return jsonify(subscription_data), 200
+
 if __name__ == "__main__":
     app.run(debug=True)
